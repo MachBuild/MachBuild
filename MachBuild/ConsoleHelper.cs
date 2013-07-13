ae89d8e8fe977a5a19b54b697af81209066f7c31
imakewins <toxic72@gmail.com>
2013-07-13 19:15:38 -0400

    First Code Commit

---
+++ MachBuild/ConsoleHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachBuild
+{
+    internal class ConsoleHelper
+    {
+        public static void WriteLineCenter(string Line)
+        {
+            Console.WriteLine("{0," + ((Console.WindowWidth / 2) + Line.Length / 2) + "}", Line);
+        }
+
+        public static void WriteCenter(string Line)
+        {
+            Console.Write("{0," + ((Console.WindowWidth / 2) + Line.Length / 2) + "}", Line);
+        }
+    }
+}

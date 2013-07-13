2118621d762b78f789cce44e94148771c4d1beb8
imakewins <toxic72@gmail.com>
2013-07-13 19:13:26 -0400

    First Code Push

---
+++ MachBuild/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.WriteLineCenter("--- MachBuild v0.0.001 ---");
+
+            Console.ReadKey();
+        }
+    }
+}
